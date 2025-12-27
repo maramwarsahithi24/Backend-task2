71bdc0cdc526fa2a4583d9444dece31e86728fd7
maramwarsahithi24 <maramwarsahithi24@gmail.com>
2025-12-27 20:51:43 +0530

    Delete auth.js

--- auth.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const {signup, loginUser} = require('../controllers/auth');
-
-router.post('/login',loginUser);
-router.post('/signup',signup);
-
-module.exports=router;
